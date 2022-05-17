599c64407c37925140b5c8be330d725d079c727b
Jana <Jana.Yousef.ruh.k@tuwaiq.edu.sa>
2022-05-17 16:33:05 +0300

    - Add Projects User

--- app/src/main/java/com/janayalsalem/profilepage/MainActivity.kt
+++ app/src/main/java/com/janayalsalem/profilepage/MainActivity.kt
@@ -5,7 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -17,8 +20,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,9 +49,7 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun CardPage() {
-    val buttonClickedState = remember {
-        mutableStateOf(false)
-    }
+    val buttonClickedState = remember { mutableStateOf(false) }
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -68,6 +71,7 @@ fun CardPage() {
             ) {
                 ProfileImage()
                 ProfileInfo()
+                ShowButton(buttonClickedState)
 
 
             }
@@ -134,4 +138,95 @@ fun ProfileInfo() {
 
 
     }
+}
+
+
+@Composable
+fun ShowButton( buttonClickedState: MutableState<Boolean>) {
+
+    Button(onClick = { buttonClickedState.value = !buttonClickedState.value }) {
+        if(buttonClickedState.value)
+        Text("Hide All Project", style = MaterialTheme.typography.button)
+    else
+        Text("Show All Project", style = MaterialTheme.typography.button)
+    }
+    if (buttonClickedState.value) {
+        ShowData()
+    }else {
+        Box {}
+    }
+}
+
+@Composable
+fun ShowData() {
+
+    val topicOfProject = listOf("Project 1", "Project 2", "Project 3", "Project 3", "Project 3")
+
+    Box(modifier = Modifier
+        .fillMaxHeight()
+        .fillMaxWidth()
+        .padding(5.dp)){
+        Surface(modifier = Modifier
+            .padding(3.dp)
+            .fillMaxWidth()
+            .fillMaxHeight(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2.dp, color = Color.LightGray)) {
+            Content(data = topicOfProject)
+
+        }
+
+    }
+
+}
+
+@Composable
+fun Content(data: List<String>) {
+    LazyColumn{
+        items(data) { item ->
+            Card(modifier = Modifier
+                .padding(13.dp)
+                .fillMaxWidth(),
+                shape = RectangleShape,
+                elevation = 4.dp) {
+
+               // all projects show as rows
+                Row(modifier = Modifier
+                    .padding(8.dp)
+                    .background(MaterialTheme.colors.surface)
+                    .padding(7.dp)) {
+
+                    Column(
+                        modifier = Modifier
+                            .padding(7.dp)
+                            .align(alignment = Alignment.CenterVertically)
+                    ) {
+                        Text(text = item, fontWeight = FontWeight.Bold)
+                        Text(text = "A great Project", style = MaterialTheme.typography.body2)
+                    }
+
+                    Column(
+                        modifier = Modifier
+                            .padding(7.dp)
+                            .align(alignment = Alignment.CenterVertically)
+                    ) {
+                        Text(text = "                           ", fontWeight = FontWeight.Bold)
+                        Text(text = "                            ", style = MaterialTheme.typography.body2)
+                    }
+
+
+                        Button(onClick = { /*TODO*/ }, modifier = Modifier
+                            .width(80.dp)
+                            .height(50.dp)) {
+                            Text("More Details", style = MaterialTheme.typography.button)
+                        }
+
+
+                }
+
+            }
+
+        }
+    }
+
 }
