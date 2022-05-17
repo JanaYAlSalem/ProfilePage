7267ddcd3a9cf697693a4edd739c3e7ce1c15267
Jana <Jana.Yousef.ruh.k@tuwaiq.edu.sa>
2022-05-17 12:32:42 +0300

    - Add Card

--- app/src/main/java/com/janayalsalem/profilepage/MainActivity.kt
+++ app/src/main/java/com/janayalsalem/profilepage/MainActivity.kt
@@ -3,13 +3,24 @@ package com.janayalsalem.profilepage
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.janayalsalem.profilepage.ui.theme.ProfilePageTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +33,39 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CardPage()
                 }
             }
         }
     }
 }
-
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ProfilePageTheme {
-        Greeting("Android")
+fun CardPage() {
+    val buttonClickedState = remember {
+        mutableStateOf(false)
     }
+    Surface( modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()) {
+        Card(modifier = Modifier
+            .width(200.dp)
+            .height(390.dp)
+            .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            elevation = 4.dp) {
+
+            Column(modifier = Modifier.height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+
+
+            }
+
+        }
+
+    }
+
 }
+
+
